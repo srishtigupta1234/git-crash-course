67dfadabd00a96919e8c467f72e6b65a90090283
Srishti Gupta <srishtigupta97527@gmail.com>
2025-07-22 18:54:41 +0530

    Added isAbove18

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const age = 24;
+const name1 = 'Piyush Garg';
+const dob = '10/12/2025';
+function sayName(){
+    console.log("My name is "+ name1); 
+}
+function isAbove18(){
+    return age>=18;
+}
+function sayHello(){
+    console.log("Hello"); 
+}
